7db1ca5c024ad8f8856cf4be470d180b091a4e1f
Nahla <nahla.nee@protonmail.com>
2024-09-27 23:32:32 -0600

    add button with radio

--- js/main.js
+++ js/main.js
@@ -4,15 +4,12 @@ window.onload = loaded;
  * Simple Function that will be run when the browser is finished loading.
  */
 function loaded() {
-    // Assign to a variable so we can set a breakpoint in the debugger!
-    const hello = sayHello();
-    console.log(hello);
+    document.getElementById("greet_btn").onclick = function() {
+        greet();
+    }
 }
 
-/**
- * This function returns the string 'hello'
- * @return {string} the string hello
- */
-export function sayHello() {
-    return 'hello';
+function greet() {
+    let greeting = document.querySelector('input[name="greeting"]:checked').value;
+    alert(greeting);
 }
